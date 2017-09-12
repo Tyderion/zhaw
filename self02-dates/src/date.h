2d1ae2fd14fa61eec51f2da3593b69cdcd0da39d
Gabriel Nadler <nadlergabriel@gmail.com>
2017-09-12 16:41:39 +0200

    Start work on dates

---
+++ self02-dates/src/date.h
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+
+typedef enum Months {
+    Jan=1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+} Month;
+
+typedef struct {
+    int day;
+    Month month;
+    int year;
+} Date;
+
+/**
+ * @brief   computes next day
+ * @param[in] date the date to copmute the next of
+ * @returns the next day
+ */
+ void next_day(Date date);
+
+ /**
+ * @brief   validates the given date
+ * @param[in] date the date to validate
+ * @returns true if the date is valid, false otherwise
+ */
+ bool valid_date(Date date);
+ 
