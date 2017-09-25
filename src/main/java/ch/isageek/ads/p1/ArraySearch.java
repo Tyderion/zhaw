001534efeac4546fae7e33265c254f14f6a3477f
Gabriel Nadler <nadlergabriel@gmail.com>
2017-09-25 14:39:02 +0200

    Add tests and some implementation

---
+++ src/main/java/ch/isageek/ads/p1/ArraySearch.java
@@ -0,0 +1,13 @@
+package ch.isageek.ads.p1;
+
+public class ArraySearch {
+    public static final int KEY_NOT_FOUND = -1;
+
+    public static int sequentialSearch(int[] array, int key) {
+        return 0;
+    }
+
+    public static long binarySearch(int[] array, int key) {
+        return 0;
+    }
+}
