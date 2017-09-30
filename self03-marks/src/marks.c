803c0fc6bcf0070bfd179d33ce0f9d126f078181
Gabriel Nadler <nadlergabriel@gmail.com>
2017-09-30 14:47:05 +0200

    add main

--- self03-marks/src/marks.c
+++ self03-marks/src/marks.c
@@ -18,11 +18,8 @@
 
 Mark get_mark(const int p, const int p6)
 {
-	//printf("points: %d, p6: %d\n", p, p6);
 	const double mark_raw = 1 + (5 * p) / (double)p6;
 	Mark rounded = floor(mark_raw + 0.5);
-	//printf("points: %d, rounded: %d\n", p, rounded);
-	//printf("Rounded: %d, raw: %f, points: %d, p6: %d\n", rounded, mark_raw, p, p6);
 	if (rounded > Six) {
 		return Six;
 	}
@@ -62,6 +59,24 @@ Statistic compute_statistics(const int *const points, const int student_count, c
 	return statistic;
 }
 
+
+void print_statistic(Statistic statistic) {
+	printf("--------------------------------------------------------\n");
+	printf("Statistics (%d students, %d points for mark 6.\n", statistic.num_students, statistic.p6);
+	printf("\n");
+	for (int grade = 1; grade <= 6; grade++) {
+		printf("Grade %d: %d\n", grade, statistic.distribution[grade-1]);
+	}
+	printf("\n");
+
+	printf("Best Mark: %d\n", statistic.best_mark);
+	printf("Worst Mark: %d\n", statistic.worst_mark);
+	printf("Average: %f\n", statistic.average);
+	printf("Mark >= 4: %d students (%d%%)\n", statistic.above_4, (int)floor(100 * (float)statistic.above_4/(float)statistic.num_students + 0.5));
+
+	printf("--------------------------------------------------------\n");
+}
+
 /**
  * @brief Main entry point.
  * @param[in] argc  The size of the argv array.
@@ -74,4 +89,26 @@ Statistic compute_statistics(const int *const points, const int student_count, c
  */
  int main(int argc, char *argv[])
  {
+	 int grades[100];
+	 int grade;
+	 int count = 0;
+	 while (true) {
+		printf("Please enter the next grade. Enter -1 to end entering grades.\n");
+		scanf("%d", &grade);
+		if (grade == -1) {
+			break;
+		}
+		if (grade < -1) {
+			printf("Students cannot score negative points.\nPlease enter positive numbers or -1 to abort.\n");
+			continue;
+		}
+		grades[count] = grade;
+		count ++;
+	 }
+	 int p6;
+	 printf("Please enter the minimal points for grade 6\n");
+	 scanf("%d", &p6);
+
+	 Statistic statistic = compute_statistics(grades, count, p6);
+	 print_statistic(statistic);
  }
