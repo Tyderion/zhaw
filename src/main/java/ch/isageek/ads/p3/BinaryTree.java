54f9d6bd29d7ea86a358cd201fec3d847eac2d77
Gabriel Nadler <nadlergabriel@gmail.com>
2017-10-06 17:49:25 +0200

    Add code from exercise zip and make it at least compile

---
+++ src/main/java/ch/isageek/ads/p3/BinaryTree.java
@@ -0,0 +1,40 @@
+package ch.isageek.ads.p3;
+
+import java.util.ArrayList;
+
+public class BinaryTree<T extends Comparable<T>> {
+
+    private TreeNode<T> root;
+
+    public BinaryTree(T value) {
+
+    }
+
+    public BinaryTree() {
+
+    }
+
+    public ArrayList<Integer> traversePostorder() {
+        return null;
+    }
+
+    public ArrayList<Integer> traverseLevelorder() {
+        return null;
+    }
+
+    public ArrayList<Integer> traversePreorder() {
+        return null;
+    }
+
+    public ArrayList<Integer> traverseInorder() {
+        return null;
+    }
+
+    public TreeNode<T> getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode<T> root) {
+        this.root = root;
+    }
+}
