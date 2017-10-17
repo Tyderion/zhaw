38a60b92d277d5b18b8e79038a478ec3ef61a0f8
Gabriel Nadler <nadlergabriel@gmail.com>
2017-10-17 16:39:47 +0200

    Add node test

--- src/test/java/ch/isageek/ads/p5/list/NodeTest.java
+++ src/test/java/ch/isageek/ads/p5/list/NodeTest.java
@@ -1,4 +1,76 @@
-package ch.isageek.ads.p5;
+package ch.isageek.ads.p5.list;
 
-public class EdgeTest {
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNull;
+
+public class NodeTest {
+
+    @Test
+    public void testAddEdge() {
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Edge e = new Edge(b, 1);
+
+
+        a.addEdge(e);
+
+        assertEquals(1, a.getEdges().size());
+    }
+
+    @Test
+    public void testGetEdge() {
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Edge e = new Edge(b, 1);
+
+
+        a.addEdge(e);
+
+        assertEquals(e, a.getEdgeTo(b));
+    }
+
+    @Test
+    public void testGetNonexistingEdge() {
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Edge e = new Edge(b, 1);
+
+
+        a.addEdge(e);
+
+        assertNull(a.getEdgeTo(a));
+    }
+
+    @Test
+    public void testRemoveEdge() {
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Edge e = new Edge(b, 1);
+
+        a.addEdge(e);
+
+        a.removeEdge(e);
+
+        assertEquals(0, a.getEdges().size());
+        assertNull(a.getEdgeTo(b));
+    }
+
+    @Test
+    public void testAddRemoveMultipleEdges() {
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Node c = new Node("c");
+        Edge e1 = new Edge(b, 1);
+        Edge e2 = new Edge(c, 1);
+
+        a.addEdge(e1);
+        a.addEdge(e2);
+
+        a.removeEdge(e1);
+
+        assertEquals(1, a.getEdges().size());
+        assertEquals(e2, a.getEdgeTo(c));
+    }
 }
