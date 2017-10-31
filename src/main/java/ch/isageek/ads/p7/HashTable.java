a32321a62ddd9ddfaf8f450df0aedb3f41db753b
Gabriel Nadler <nadlergabriel@gmail.com>
2017-10-31 14:28:04 +0100

    Add interface for HashTable

---
+++ src/main/java/ch/isageek/ads/p7/HashTable.java
@@ -0,0 +1,58 @@
+package ch.isageek.ads.p7;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public interface HashTable<T> {
+
+    /**
+     * Returns number of elements in the hashtable
+     * @return the number of elements in the hashtable
+     */
+    int size();
+
+    /**
+     * Returns true if the table is empty
+     * @return true if the table is empty, false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Adds an element to the table
+     * @param element The object to add
+     */
+    void add(T element);
+
+    /**
+     * Tests whether the element exists in the table
+     * @param element The element to find
+     * @return true if the hashtable contains the element, false otherwise
+     */
+    boolean contains(T element);
+
+    /**
+     * Removes the given element from the hashtable
+     * @param element the element to remove
+     * @return true if the element was in the table, false otherwise
+     */
+    boolean remove(T element);
+
+    /**
+     * Set the loadfactor which the table uses to grow
+     * @param loadfactor the loadfactor, should be between 0 and 1 (values above will be 1, values below will be 0)
+     */
+    void setLoadFactorForResize(float loadfactor);
+
+    /**
+     * Returns an {@link Iterator} over all elements in the hashtable.
+     * The {@link Iterator} iterates over the elements in the order they are stored.
+     * @return An {@link Iterator} over all elements in the hashtable
+     */
+    Iterator<T> iterator();
+
+    /**
+     * Add all elements of the given {@link Collection} to the hashtable
+     * @param elements the {@link Collection} of elements to add
+     */
+    void addAll(Collection<T> elements);
+}
