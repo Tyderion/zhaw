b5360b72ef6ee3f428d2bc7140af72dd38af8afe
Gabriel Nadler <nadlergabriel@gmail.com>
2017-11-07 11:18:06 +0100

    InsertionSort

--- src/main/java/ch/isageek/ads/p8/InsertionSort.java
+++ src/main/java/ch/isageek/ads/p8/InsertionSort.java
@@ -3,6 +3,19 @@ package ch.isageek.ads.p8;
 public class InsertionSort implements Sorter {
     @Override
     public void sort(int[] numbers) {
-
+        if (numbers == null || numbers.length == 1) {
+            return;
+        }
+        int i = 1;
+        while (i < numbers.length) {
+            int ele = numbers[i];
+            int j = i - 1;
+            while (j >= 0 && numbers[j] > ele) {
+                numbers[j + 1] = numbers[j];
+                j = j - 1;
+            }
+            numbers[j + 1] = ele;
+            i++;
+        }
     }
 }
