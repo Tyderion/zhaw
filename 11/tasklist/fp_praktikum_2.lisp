180dfdfb7ca99788eba1749babab6be626353b63
Gabriel Nadler <nadlergabriel@gmail.com>
2018-12-01 13:48:35 +0100

    remove duplicate function

--- 11/tasklist/fp_praktikum_2.lisp
+++ 11/tasklist/fp_praktikum_2.lisp
@@ -7,19 +7,6 @@
       data)))
 
 
-;; Alternative implementation in case the Common Lisp used does not
-;; support the read-sequence function
-;;
-(defun file-to-string (path)
-  (with-open-file (stream path)
-    (apply #'concatenate 
-           (cons 'string 
-                 (loop
-                   for line = (read-line stream nil 'eof)
-                   until (eq line 'eof)
-                   collect (format nil "~A~%" line))))))
-
-
 ;; Split string at character
 ;;
 (defun string-split (c strg &optional (start 0))
