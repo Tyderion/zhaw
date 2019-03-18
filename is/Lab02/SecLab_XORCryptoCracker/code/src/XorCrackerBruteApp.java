4b48445c8babe129d4283c91496623c28901a3e0
Gabriel Nadler <nadlergabriel@gmail.com>
2019-03-18 12:02:36 +0100

    Crack XOR

--- is/Lab02/SecLab_XORCryptoCracker/code/src/XorCrackerBruteApp.java
+++ is/Lab02/SecLab_XORCryptoCracker/code/src/XorCrackerBruteApp.java
@@ -1,16 +1,16 @@
+import ch.zhaw.init.is.crypto.classic.ByteFrequencyTable;
+import ch.zhaw.init.is.crypto.classic.ByteFrequencyTableHelpers;
+import ch.zhaw.init.is.util.HexTools;
+
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import ch.zhaw.init.is.crypto.classic.ByteFrequencyTable;
-import ch.zhaw.init.is.crypto.classic.ByteFrequencyTableHelpers;
-import ch.zhaw.init.is.util.HexTools;
-
 /**
- * Application for cracking the encryption of files that have been 
+ * Application for cracking the encryption of files that have been
  * encrypted with the XOR method with key lengths much smaller than
- * the size of the plaintext. Cracking is based on the assumption 
+ * the size of the plaintext. Cracking is based on the assumption
  * that there is a byte value in the plaintext which is significantly more
  * frequent than other byte values.
  * <p>
@@ -25,16 +25,16 @@ import ch.zhaw.init.is.util.HexTools;
  * <p>
  * @author tebe
  */
-public class XorCrackerApp {
+public class XorCrackerBruteApp {
 	private static int keylength;
 	private static String filename;
 
 	/**
 	 * Main method of the application
-	 * 
+	 *
 	 * @param args
 	 *            Command line arguments
-	 * 
+	 *
 	 */
 	public static void main(String[] args) {
 		if (parseCommandLineParameters(args)) {
@@ -54,7 +54,7 @@ public class XorCrackerApp {
 
 	/**
 	 * Checks and parses the command line arguments.
-	 * 
+	 *
 	 * @param args
 	 *            Command line arguments
 	 * @return true, if the arguments could be parsed
@@ -71,7 +71,7 @@ public class XorCrackerApp {
 		return false;
 	}
 
-	
+
 	/**
 	 * Starts and controls the analysis process
 	 * @throws IOException
@@ -79,37 +79,40 @@ public class XorCrackerApp {
 	private static void run() throws IOException {
 		try {
 			System.out.println("Number of candidate keys: " + getNumberOfCandidateKeys());
-			int[] key = getCandidateKey(filename, keylength, 0x65);
-			System.out.println(HexTools.intArrayToHexString(key));
+			for(int c = 0; c <= getNumberOfCandidateKeys(); c++){
+				int[] key = getCandidateKey(filename, keylength, c);
+				System.out.println(HexTools.intArrayToHexString(key));
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static int getNumberOfCandidateKeys() {
-		return 1;
+		return 127;
 	}
 
-	
+
 	/**
 	 * Determines the key assuming the specified most frequent byte is the most frequent
-	 * byte in the plaintext and keylength is the true length of the key. 
-	 * 
-	 * @param filename 
+	 * byte in the plaintext and keylength is the true length of the key.
+	 *
+	 * @param filename
 	 *            The name of the file to process
 	 * @param keylength
 	 *            The key length in bytes
 	 * @param mostFrequentValue
 	 *            The assumed most frequent byte in the plaintext
-	 *            
+	 *
 	 * @return The key
 	 * @throws IOException
 	 */
 	private static int[] getCandidateKey(String filename, int keylength, int mostFrequentValue) throws IOException {
 		DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));
-		ByteFrequencyTable[] frequencyTable = ByteFrequencyTableHelpers.getFrequencyTableForKeyLength(keylength, inputStream); 
+		ByteFrequencyTable[] frequencyTable = ByteFrequencyTableHelpers.getFrequencyTableForKeyLength(keylength, inputStream);
 		int[] key = new int[keylength];
 		for(int keyByte = 0; keyByte < keylength; keyByte++){
+
 			//TODO: Determine byte 'keyByte' of the key
 			key[keyByte] = frequencyTable[keyByte].getMostFrequentByte() ^ mostFrequentValue;
 		}
