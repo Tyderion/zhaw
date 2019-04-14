194ebe0c004d6a3452eb8b822b7ee7dd0f558c2e
Gabriel Nadler <gabriel.nadler@gmail.com>
2019-04-14 09:18:30 +0200

    Lab04 from olat

---
+++ bsy/p04/Mishell/SiShell/readline.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+// File:    readline.c
+// Fach:    BSy
+// Autor:   M. Thaler
+// Version: v.fs19
+******************************************************************************/
+
+#include <stdio.h>
+
+/*****************************************************************************/
+/* readline reads at most count-1 characters from stdin into the buffer buf  */
+/* returns the number of characters (! must be less/equal : count-1          */
+
+int readline(char *buf, int count) {
+
+    return 0;
+}
+
+/*****************************************************************************/
