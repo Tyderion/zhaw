ce32a87ef19704aebeaa9f40207b269c813beb90
stephaniebernhard <fanny.bernhard@gmail.com>
2019-10-02 16:12:37 +0200

    named things

--- scc/Week02/Code/earth_sun3.m
+++ scc/Week02/Code/earth_sun3.m
@@ -1,30 +1,31 @@
 function earth_sun3()
-    ce_start = [5; 0]; 
+    % define centers
+    center_earth_start = [5; 0]; 
+    center_sun = [0; 0];
 
-    sun = [0; 0]; %coordinates of the sun
+    % angle for earth orbiting the sun (arbitrarily chosen number)
+    phi_rotate =  pi/180;
+    % number of iterations
+    n = 1000; 
 
-    Phi_rotate =  pi/180; %angle for earth orbiting the sun (arbitrarily chosen number)
-    n = 300; %number of iterations
-
-    % Rectangle Coords
-    earth = create_thing_at(ce_start, 1);
+    % rectangle coords
+    earth = create_thing_at(center_earth_start, 1);
 
     for i = 1:n
-        %earth orbiting the sun
-        earth = rotate_thing_around(earth, sun, Phi_rotate);
+        % earth orbiting the sun
+        earth = rotate_thing_around(earth, center_sun, phi_rotate);
         
-        % Spin things
-        earth = spin_thing(earth, Phi_rotate);
+        % spin things
+        earth = spin_thing(earth, phi_rotate);
         
-        %plot
+        % plot init
         clf
         hold on
-        % Earth
+        % plot earth
         draw_thing(earth, 'red')
-        
-        %Sun
-        plot(sun(1), sun(2), 'y*');
-
+        %plot sun
+        plot(center_sun(1), center_sun(2), 'y*');
+        % plot settings
         set(gca,'Color','k')
         axis([-10, 10, -10, 10]);
         pause(0.0005);
--- scc/Week02/Code/earth_sun_moon.m
+++ scc/Week02/Code/earth_sun_moon.m
@@ -1,38 +1,41 @@
 function earth_sun_moon()
     earth_start = [5; 0];
     moon_start = [earth_start(1) + 2; earth_start(2)];
-    sun = [0; 0]; %coordinates of the sun
+    sun_center = [0; 0]; %coordinates of the sun
+    
+    % angle for earth orbiting the sun (arbitrarily chosen number)
+    phi_rotate =  pi/180;
+    % angle for moon orbiting the earth (arbitrarily chosen number)
+    phi_rotate_moon = phi_rotate*2; 
+    % number of iterations
+    n = 1000; 
 
-    Phi_rotate =  pi/180; %angle for earth orbiting the sun (arbitrarily chosen number)
-    Phi_rotate_moon = Phi_rotate*2; %angle for moon orbiting the earth (arbitrarily chosen number)
-    n = 1000; %number of iterations
-
-    % Create moon/earth
+    % create moon/earth
     earth = create_thing_at(earth_start, 1);
     moon = create_thing_at(moon_start, 0.5);
 
     for i = 1:n
         %earth orbiting the sun
-        earth = rotate_thing_around(earth, sun, Phi_rotate);
+        earth = rotate_thing_around(earth, sun_center, phi_rotate);
 
         %moon orbiting the earth
-        moon = rotate_thing_around(moon, earth.center, Phi_rotate_moon);
+        moon = rotate_thing_around(moon, earth.center, phi_rotate_moon);
 
         %moon orbiting the sun (keep with earth)        
-        moon = rotate_thing_around(moon, sun, Phi_rotate);
+        moon = rotate_thing_around(moon, sun_center, phi_rotate);
 
-        % Spin things
-        earth = spin_thing(earth, Phi_rotate);
-        moon = spin_thing(moon, Phi_rotate_moon);
+        % spin things
+        earth = spin_thing(earth, phi_rotate);
+        moon = spin_thing(moon, phi_rotate_moon);
 
-        %plot
+        % plot init
         clf
         hold on
+        % plot earth/moon/sun
         draw_thing(earth, 'red')
         draw_thing(moon, 'green')
-        plot(sun(1), sun(2), 'y*');
-
-        % Plot settings
+        plot(sun_center(1), sun_center(2), 'y*');
+        % plot settings
         set(gca,'Color','k')
         axis([-10, 10, -10, 10]);
         pause(0.0005);
