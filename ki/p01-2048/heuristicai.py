9fe0e442de3e626d2bcffe6f79822098022e5d4c
Gabriel Nadler <gabriel.nadler@gmail.com>
2019-10-06 15:38:11 +0200

    Cleanup heuristic

--- ki/p01-2048/heuristicai.py
+++ ki/p01-2048/heuristicai.py
@@ -1,10 +1,7 @@
 import random
 import game
 import sys
-from enum import Enum
 import numpy as np
-import copy
-import multiprocessing
 
 # Author:				chrn (original by nneonneo)
 # Date:				11.11.2016
@@ -12,207 +9,65 @@ import multiprocessing
 
 UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
 NO_MOVE = -1
+MIN_SCORE = -10 ** 12
 
 EMPTY = 0
 
-flatten = lambda l: [item for sublist in l for item in sublist]
 
 def find_best_move(board):
-    bestmove = -1    
-	
-	# TODO:
-	# Build a heuristic agent on your own that is much better than the random agent.
-	# Your own agent don't have to beat the game.
+    best_move = NO_MOVE
+    best_score = MIN_SCORE
+
+    for move in range(4):
+        new_board = execute_move(move, board)
+        new_score = score(new_board)
+        if new_score > best_score:
+            best_score = new_score
+            best_move = move
+
+    return best_move
 
 
-    bestmove = par_simple(board, 2)
-    #bestmove = heuristic_simple(board, 3, True)
-    if board_equals(board, execute_move(bestmove[0], board)):
-        move = find_best_move_random_agent()
-        while move == bestmove[0]:
-            move = find_best_move_random_agent()
-        return move
-    return bestmove[0]
+# weights = [[1, 0, 0, 1],
+#            [0, -1, -1, 0],
+#            [0, -1, -1, 0],
+#            [1, 0, 0, 1]]
+#
+# weights = [[3, 2, 2, 3],
+#            [2, 0, 0, 2],
+#            [2, 0, 0, 2],
+#            [3, 2, 2, 3]]
+# weights = [[2, 1, 1, 1],
+#            [2, -1, -1, 1],
+#            [2, -1, -1, 1],
+#            [2, 2, 2, 2]]
+weights = np.array([
+    [1, 1, 1, 1],
+    [1, 0.1, 0.1, 1],
+    [1, 0.1, 0.1, 1],
+    [1, 1, 1, 1]
+])
+
+
+def score(board):
+    free_spots = count_zeros(board) + 1
+    result = (weights * board).sum()
+
+    return result * free_spots
+
+
+def count_zeros(board):
+    return 16 - np.count_nonzero(board)
+
 
 def find_best_move_random_agent():
-    return random.choice([UP,DOWN,LEFT,RIGHT])
-
-def get_next_merge_count(board, direction):
-    b = copy.deepcopy(board)
-    return 0
-
-def get_empty_count(board):
-    result = 0
-    for row in range(4):
-        for col in range(4):
-            if board[row][col] == 0:
-                result += 1
-    return result
-
-def par_simple(board, depth):
-    jobs = []
-    manager = multiprocessing.Manager()
-    return_dict = manager.dict()
-    rowmerges = flatten([count_row_merges(board, row) for row in range(4)])
-    colmerges = flatten([count_col_merges(board, col) for col in range(4)])
-    highest_move = get_highest_merge(rowmerges, colmerges)
-    if highest_move.direction(board) != NO_MOVE:
-        return [highest_move.direction(board), highest_move.number]
-    for move in [UP, DOWN, LEFT, RIGHT]:
-        # Declare a new process and pass arguments to it
-        new_board = execute_move(move, board)
-        p1 = multiprocessing.Process(target=worker, args=(move, new_board, depth, return_dict))
-        jobs.append(p1)
-        p1.start()
-
-    for proc in jobs:
-        proc.join()
-
-    best_move = [NO_MOVE, -1]
-    for move in return_dict.values():
-        if move[1] > best_move[1]:
-            best_move = move
-    return best_move
-
-def worker(move, board, depth, result):
-    prediction = heuristic_simple(board, depth, True)
-    result[move] = prediction
-
-def heuristic_simple(board, depth, predict):
-    rowmerges = flatten([count_row_merges(board, row) for row in range(4)])
-    colmerges = flatten([count_col_merges(board, col) for col in range(4)])
-    best_move = [LEFT, 0]
-    highest_move = get_highest_merge(rowmerges, colmerges)
-    if highest_move.direction(board) != NO_MOVE:
-        return [highest_move.direction(board), highest_move.number]
-    if depth == 0:
-        return max_merges(rowmerges, colmerges)
-    for move in [UP, DOWN, LEFT, RIGHT]:
-        new_board = execute_move(move, board)
-        if predict: #and get_empty_count(new_board) < 6:
-            count = 0
-            sum_count = 0
-            for row in range(4):
-                for col in range(4):
-                    if new_board[row][col] == 0:
-                        # TODO: Include 90% chance of new 2
-                        count += 1
-                        new_board[row][col] = 2
-                        sum_count += heuristic_simple(new_board, depth - 1, predict)[1]
-                        # new_board[row][col] = 4
-                        # sum_count += heuristic_simple(new_board, depth - 1)[1]
-                        new_board[row][col] = 0
-            current = [move, 0 if count == 0 else sum_count / count]
-        else:
-            current = heuristic_simple(new_board, depth - 1, predict)
-        if current[1] > best_move[1]:
-            best_move = current
-    return best_move
-
-def max_merges(row_merges, col_merges):
-    rows = len(row_merges)
-    cols = len(col_merges)
-    return [LEFT, rows] if rows > cols else [DOWN, cols]
-
-class Move:
-    def __init__(self, horizontal, index, first, second, number):
-        self.horizontal = horizontal
-        self.index = index
-        self.first = first
-        self.second = second
-        self.number = number
-
-    def direction(self, board):
-        if self.number == -1:
-            return NO_MOVE
-        if self.horizontal:
-            return LEFT
-            # if self.first == 0:
-            #     return LEFT
-            # elif self.second == 3:
-            #     return RIGHT
-            # elif board[self.index][self.first] == EMPTY:
-            #     return LEFT
-            return RIGHT
-        # if self.first == 0:
-        #     return UP
-        # elif self.second == 3:
-        #     return DOWN
-        # elif board[self.first][self.index] == EMPTY:
-        #     return UP
-        return DOWN
-
-    def __repr__(self):
-        return "{{{}Move{}}}".format("Horizontal" if self.horizontal else "Vertical", self.__str__())
-    def __str__(self):
-        if self.horizontal:
-            return "({}, {})-({}, {}): {}".format(self.index, self.first, self.index, self.second, self.number)
-        return "({}, {})-({}, {}): {}".format(self.first, self.index, self.second, self.index, self.number)
+    return random.choice([UP, DOWN, LEFT, RIGHT])
 
 
-no_move = Move(True, 0, 0, 0, -1)
-def get_highest_merge(row_merges, col_merges):
-    limit = 15
-    rows = [ele for ele in row_merges if ele.number > limit]
-    cols = [ele for ele in col_merges if ele.number > limit]
-
-    maxrow = no_move if len(rows) == 0 else max(rows, key=lambda item: item.number)
-    maxcol = no_move if len(cols) == 0 else max(cols, key=lambda item: item.number)
-    if maxrow.number == maxcol.number:
-        return no_move
-    elif maxrow.number > maxcol.number:
-        return maxrow
-    else:
-        return maxcol
-
-def get_merge_moves(board):
-    rows = flatten([count_row_merges(board, row) for row in range(4)])
-    cols = flatten([count_col_merges(board, col) for col in range(4)])
-    if not rows and not cols:
-        return NO_MOVE
-    if not rows:
-        return cols[0].direction(board)
-    if not cols:
-        return rows[0].direction(board)
-    if len(rows) >= len(cols):
-        return max(rows, key=lambda item: item.number).direction(board)
-    return max(rows, key=lambda item: item.number).direction(board)
-
-def count_row_merges(board, row):
-    result = []
-    current = -1
-    for i in range(4):
-        if board[row][i] == EMPTY:
-            continue
-        if current == -1:
-            current = i
-            continue
-        if board[row][current] == board[row][i]:
-            result.append(Move(True, row, current, i, board[row][i]))
-        else:
-            current = i
-    return result
-
-def count_col_merges(board, col):
-    result = []
-    current = -1
-    for i in range(4):
-        if board[i][col] == EMPTY:
-            continue
-        if current == -1:
-            current = i
-            continue
-        if board[current][col] == board[i][col]:
-            result.append(Move(False, col, current, i, board[i][col]))
-        else:
-            current = i
-    return result
-
-    
 def execute_move(move, board):
     """
     move and return the grid without a new random tile 
-	It won't affect the state of the game in the browser.
+    It won't affect the state of the game in the browser.
     """
 
     if move == UP:
@@ -225,9 +80,10 @@ def execute_move(move, board):
         return game.merge_right(board)
     else:
         sys.exit("No valid move")
-		
+
+
 def board_equals(board, newboard):
     """
     Check if two boards are equal
     """
-    return  (newboard == board).all()  
+    return (newboard == board).all()
