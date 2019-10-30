1e3f54814c4829bd2644a4d902a0abde30d59621
Gabriel Nadler <gabriel.nadler@gmail.com>
2019-10-30 16:39:09 +0100

    CSP

---
+++ ki/P03_CSP_Datalog/sudoku_TASK.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 04 08:13:32 2017
+
+Formulates sudoku as a CSP, solving the riddle from
+https://www.sudoku.ws/hard-1.htm as an example.
+
+@author: stdm
+@modif: tugg
+"""
+
+import sys
+sys.path.append("./python-constraint-1.2")
+import constraint as csp
+
+# ------------------------------------------------------------------------------
+# sudoku to solve (add "0" where no number is given)
+# ------------------------------------------------------------------------------
+riddle = [[0,0,0,2,0,0,0,6,3],
+             [3,0,0,0,0,5,4,0,1],
+             [0,0,1,0,0,3,9,8,0],
+             [0,0,0,0,0,0,0,9,0],
+             [0,0,0,5,3,8,0,0,0],
+             [0,3,0,0,0,0,0,0,0],
+             [0,2,6,3,0,0,5,0,0],
+             [5,0,3,7,0,0,0,0,8],
+             [4,7,0,0,0,1,0,0,0]]
+
+# ------------------------------------------------------------------------------
+# create helpful lists of variable names
+# ------------------------------------------------------------------------------
+rownames = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+colnames = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+rows = []
+for i in rownames:
+    row = []
+    for j in colnames:
+        row.append(i+j)
+    rows.append(row)
+
+cols = []
+for j in colnames:
+    col = []
+    for i in rownames:
+        col.append(i+j)
+    cols.append(col)
+
+boxes = []
+for x in range(3):  # over rows of boxes
+    for y in range(3):  # over columns of boxes
+        box = []
+        for i in range(3):  # over variables in rows (in a box)
+            for j in range(3):  # over variables in cols (in a box)
+                box.append(rownames[x*3 + i] + colnames[y*3 + j])
+        boxes.append(box)
+
+
+# ------------------------------------------------------------------------------
+# formulate sudoku as CSP
+# ------------------------------------------------------------------------------
+sudoku = csp.Problem()
+
+sudoku.addVariables([ele for row in rows for ele in row], [1, 2, 3, 4, 5, 6, 7, 8, 9] )
+for row in range(len(riddle)):
+    for col in range(len(riddle[row])):
+        if riddle[row][col] != 0:
+            value = riddle[row][col]
+            sudoku.addConstraint(csp.InSetConstraint({value}), [rows[row][col]])
+
+for row in rows:
+    sudoku.addConstraint(csp.AllDifferentConstraint(), row)
+for col in cols:
+    sudoku.addConstraint(csp.AllDifferentConstraint(), col)
+for box in boxes:
+    sudoku.addConstraint(csp.AllDifferentConstraint(), box)
+
+
+# ------------------------------------------------------------------------------
+# solve CSP
+# ------------------------------------------------------------------------------
+solutions = sudoku.getSolutions()
+for solution in solutions:
+    for i in rownames:
+        print('| ', end='')
+        for j in colnames:
+            print(solution[i+j], '|', end = ' ')
+        print('')
+
